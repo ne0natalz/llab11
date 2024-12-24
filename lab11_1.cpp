43eb28e4f894898c782c7139acbb7cae28fd146f
ne0natalz <phacharapalawong3@gmail.com>
2024-12-24 23:36:13 +0700

    lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    char ch;
+    for (int i = 0; i < 3; i++) {
+        cin.get(ch);
+    }
+
+    srand(static_cast<unsigned int>(time(0)));
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}
